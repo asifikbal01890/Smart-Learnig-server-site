fb7defb9cd4df96ff417e83517e3e584830b3893
asifilbal01890 <asifikbal01890@gmail.com>
2022-10-26 10:32:51 +0600

    cors install

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
-const express = require('express')
+const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) =>{
     res.send('Server Api Running');
@@ -11,6 +15,10 @@ app.get('/', (req, res) =>{
 
 app.get('/categories', (req, res)=>{
     res.send(categories);
+});
+
+app.get('/course', (req, res)=>{
+    res.send(course);
 });
 
 app.listen(port, () => {
