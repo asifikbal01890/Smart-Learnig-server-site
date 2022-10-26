aec06094b334ddc67a8a338d0143f6522009d558
asifilbal01890 <asifikbal01890@gmail.com>
2022-10-26 14:28:49 +0600

    change image url

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
 const course = require('./data/course.json');
 
 app.get('/', (req, res) =>{
     res.send('Server Api Running');
 });
 
-app.get('/categories', (req, res)=>{
-    res.send(categories);
+app.get('/course', (req, res) =>{
+    res.send(course);
 });
 
-app.get('/course', (req, res)=>{
-    res.send(course);
+app.get('/course/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c._id === id);
+    res.send(selectedCourse);
 });
 
 app.listen(port, () => {
